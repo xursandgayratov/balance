bfb35b1a09fe123883d5dbbd38a55e2171e87b94
Xursand <xursandgayratov397@gmail.com>
2026-03-10 17:47:02 +0500

    fix

---
+++ src/Main.java
@@ -0,0 +1,21 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int[] array = {
+                13, 16, 21, 42, 11, 9,
+        };
+        int sum = 0;
+        for (int i = 1; i < array.length; i++) {
+            sum += array[i];
+            {
+
+            }
+
+        }
+        System.out.println(sum/
+                array.length);
+    }
+}
+
+
+
